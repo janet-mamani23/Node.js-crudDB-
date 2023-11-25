47d3ec70e93d0f4afe09c923f1b0f78f11a28a64
Janet <janet.mamani@itscordoba.edu.ar>
2023-11-24 21:19:42 -0300

    se solucion la conexion al servidor

--- db.js
+++ db.js
@@ -6,38 +6,63 @@ const db = mysql.createConnection({
     user: 'root',
     password: '1234',
 });
-    
-db.connect((err)=> {
-    if (err){
-        console.log("Error en la consola al servidor");
+
+//Conectarnos al servidor
+db.connect((err) => {
+    if (err) {
+        console.log("Error en la conexion al server", err);
         return;
     }
-    db.query("CREATE DATABASE IF NOT EXISTS CRUD-APP",(err)=>{
-        if (err){
-            console.log("error al crear la db");
+
+    // Verificar si existe la base de datos
+    db.query("CREATE DATABASE IF NOT EXISTS cruddb2", (err) => {
+        if (err) {
+            console.log("Error al crear la db");
             return;
         }
-        console.log("DB creada o ya existe");
+        console.log("DB creada o ya existente");
     });
-    db.query("USE CRUD-APP",(err) => {
-        if(err){
-            console.log("error al seleccionar la db");
+
+    // Seleccionar base de datos
+    db.query("USE cruddb2", (err) => {
+        if (err) {
+            console.log("Error al seleccionar la db");
             return;
         }
-        console.log("conexion exitosa");
+        console.log("Conexion exitosa");
     });
-    const create_TableSQL = `
+    const createTableSQLoficina = `
+        CREATE TABLE IF NOT EXISTS oficina (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            denominacion VARCHAR(255)            
+        ) 
+    `;
+
+    // Verificar si existe la tabla persona
+    const createTableSQLpersona = `
         CREATE TABLE IF NOT EXISTS persona (
             id INT AUTO_INCREMENT PRIMARY KEY,
             nombre VARCHAR(255),
-            email VARCHAR(255)
-            
-        )   `;
-    db.query(create_TableSQL,(err) => {
-        if(err){
-            console.log("Error al crear tabla");
+            email VARCHAR(255),
+            oficina_id INT,
+            FOREIGN KEY (oficina_id) REFERENCES oficina(id)
+        ) 
+    `;
+
+    db.query(createTableSQLoficina, (err) => {
+        if (err) {
+            console.log("Error al crear la tabla");
             return;
         }
     });
+    db.query(createTableSQLpersona, (err) => {
+        if (err) {
+            console.log("Error al crear la tabla");
+            return;
+        }
+    });
+
+
 });
+
 module.exports = db;
